e53d264545d684e62806fb8c962212d954dbc2db
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-07-19 10:19:56 +0630

    todo item complete line through work with local var data

---
+++ src/components/TodoItem.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ?
+            'line-through' : 'none'
+        }
+    }
+
+    markComplete = (e) =>{
+        console.log(this.props);
+    }
+
+    render() {
+        const { id, title} = this.props.todo;
+        return (
+            <div style={this.getStyle()}>
+                <p> 
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
+                    {this.props.todo.title}
+                </p>
+            </div>
+        )
+    }
+}
+
+const itemStyle = {
+    backgroundColor : '#f4f4f4'
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default TodoItem
